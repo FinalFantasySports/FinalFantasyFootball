e31bc6b819d1b32c76d51345f5d1e0fb0dad893e
Danjenator <danjenator@gmail.com>
2018-09-18 17:25:49 -0500

    starting on getting stat key

---
+++ src/main/java/com/finalfantasy/football/stats/StatsController.java
@@ -0,0 +1,27 @@
+package com.finalfantasy.football.stats;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping(value = "/stats")
+public class StatsController {
+
+  private final StatsService statsService;
+
+  public StatsController(final StatsService statsService) {
+    this.statsService = statsService;
+  }
+
+  @RequestMapping
+  public String populateDatabaseBySeasonAndWeek() {
+    try {
+      return "We did it! " + statsService.populateDatabaseBySeasonAndWeek();
+    } catch (IOException e) {
+      e.printStackTrace();
+      return "Damn Jenna";
+    }
+  }
+}
