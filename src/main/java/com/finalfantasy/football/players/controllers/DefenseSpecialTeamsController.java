7d465ae5a1d344ff3302d75d9c9199cf7aaad405
Danjenator <danjenator@gmail.com>
2018-10-13 22:34:52 -0500

    enabled cors

--- src/main/java/com/finalfantasy/football/players/controllers/DefenseSpecialTeamsController.java
+++ src/main/java/com/finalfantasy/football/players/controllers/DefenseSpecialTeamsController.java
@@ -4,13 +4,11 @@ import com.finalfantasy.football.players.models.DefenseSpecialTeams;
 import com.finalfantasy.football.players.services.DefenseSpecialTeamsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/defensespecialteamss")
 public class DefenseSpecialTeamsController {
--- src/main/java/com/finalfantasy/football/players/controllers/WideReceiverController.java
+++ src/main/java/com/finalfantasy/football/players/controllers/WideReceiverController.java
@@ -4,13 +4,11 @@ import com.finalfantasy.football.players.models.WideReceiver;
 import com.finalfantasy.football.players.services.WideReceiverService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/widereceivers")
 public class WideReceiverController {
