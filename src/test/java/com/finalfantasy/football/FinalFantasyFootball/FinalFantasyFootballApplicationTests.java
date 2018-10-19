70627d06a7d01d200466eee3d013bc83834d0b31
Danjenator <danjenator@gmail.com>
2018-10-19 17:23:33 -0500

    migrated to gradle

--- src/test/java/com/finalfantasy/football/FinalFantasyFootball/FinalFantasyFootballApplicationTests.java
+++ src/test/java/com/finalfantasy/football/FinalFantasyFootball/FinalFantasyFootballApplicationTests.java
@@ -1,4 +1,4 @@
-package com.finalfantasy.football;
+package com.finalfantasy.football.FinalFantasyFootball;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FootballApplicationTests {
+public class FinalFantasyFootballApplicationTests {
 
 	@Test
 	public void contextLoads() {
