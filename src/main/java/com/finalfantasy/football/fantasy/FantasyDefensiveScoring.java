c392bf13623b35eb910ecdf2edc103466d4d219e
Daniel Brown <Daniel.Brown@daugherty.com>
2019-01-28 16:36:18 -0600

    added scoring models

---
+++ src/main/java/com/finalfantasy/football/fantasy/FantasyDefensiveScoring.java
@@ -0,0 +1,21 @@
+package com.finalfantasy.football.fantasy;
+
+public class FantasyDefensiveScoring {
+  int sack = 1;
+  int interception = 2;
+  int fumbleRecovery = 2;
+  int touchdown = 6;
+  int safety = 2;
+  int blockKick = 2;
+  int returnYds = 10;
+  int returnTds = 6;
+  int pointsAllowed0 = 10;
+  int pointsAllowed1_6 = 7;
+  int pointsAllowed7_13 = 4;
+  int pointsAllowed14_20 = 1;
+  int pointsAllowed21_27 = 0;
+  int pointsAllowed28_34 = -1;
+  int pointsAllowed35Plus = -4;
+  int fourthDownStop = 1;
+  int extraPointReturned = 2;
+}
---
+++ src/main/java/com/finalfantasy/football/fantasy/FantasyOffensiveScoring.java
@@ -0,0 +1,22 @@
+package com.finalfantasy.football.fantasy;
+
+public class FantasyOffensiveScoring {
+  int passingYards = 25;
+  int passingTds = 4;
+  int interceptions = -1;
+  int sacks = -1;
+  int rushingYards = 10;
+  int rushingTds = 6;
+  int receptions = 1;
+  int receivingYds = 10;
+  int receivingTds = 6;
+  int returnYds = 0;
+  int returnTds = 6;
+  int twoPointConvesion = 2;
+  int fumblesLost = -2;
+  int offensiveFumbleReturnTD = 6;
+  int fieldGoal0_39 = 3;
+  int fieldGoal40_49 = 4;
+  int fieldGoal50Plus = 5;
+  int extraPoint = 1;
+}
